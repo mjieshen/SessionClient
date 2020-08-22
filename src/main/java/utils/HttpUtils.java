3e0d13b566420a716799526e8a5213f549877082
shenjie <mjieshen@163.com>
2020-08-22 18:13:21 +0800

    add dependencies.

--- src/main/java/utils/HttpUtils.java
+++ src/main/java/utils/HttpUtils.java
@@ -1,22 +1,25 @@
 package utils;
 
-public class HttpUtils {
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
-    // TODO:
+public class HttpUtils {
     public static String sendHttpPost(String url, String requestBody) throws Exception {
-//        CloseableHttpClient httpClient = HttpClients.createDefault();
-//        HttpPost httpPost = new HttpPost(url);
-//        httpPost.addHeader("Content-Type", "application/json");
-//        httpPost.setEntity(new StringEntity(requestBody));
-//        CloseableHttpResponse response = httpClient.execute(httpPost);
-////		System.out.println(response.getStatusLine().getStatusCode() + "\n");
-//        HttpEntity entity = response.getEntity();
-//        String responseContent = EntityUtils.toString(entity, "UTF-8");
-////		System.out.println(responseContent);
-//        response.close();
-//        httpClient.close();
-//        return responseContent;
-        return null;
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.addHeader("Content-Type", "application/json");
+        httpPost.setEntity(new StringEntity(requestBody));
+        CloseableHttpResponse response = httpClient.execute(httpPost);
+        HttpEntity entity = response.getEntity();
+        String responseContent = EntityUtils.toString(entity, "UTF-8");
+        response.close();
+        httpClient.close();
+        return responseContent;
     }
 
 }
